bfe7e33731e104721abe07d69f86d5d1b437dd14
Alwin <alwang18@hotmail.com>
2018-04-25 14:56:59 +1000

    Git Project Setup
    Set up folders, save locations, etc

---
+++ bash/loop-semtex.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#============================
+# Loop over session files and solve using semtex dns
+# Alwin Wang
+#----------------------------
+
+#--- Inputs ----
+usage="$(loop-semtex "$0") [-h] -- script to loop over session files in a folder"
+# write getops case handing to display for -h
+# Input 1: folder location
+
+#--- Loop over files in folder ----
+
+
