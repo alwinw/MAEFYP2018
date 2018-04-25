c8fbbdd2da73dc7f69c1d9e28b83d2f78a8e3694
Alwin <alwang18@hotmail.com>
2018-04-25 22:13:55 +1000

    R library manager
    Completed script file

---
+++ R/src_library-manager.R
@@ -0,0 +1,35 @@
+#============================
+# Library Manager
+# Alwin Wang
+#----------------------------
+
+#--- Recommendations ----
+# R version: 64-bit https://cran.r-project.org/bin/windows/base/
+# RStudio: Free Versions https://www.rstudio.com/products/rstudio/download/
+# Use RStudio projects to manage code and workspace
+
+#--- List of  Required Packages ----
+# List of packages required
+packages <- c(
+  "stats",      # For statistical analysis, Part of R Core
+  "ggplot2",    # Great for visualising data, by Adj. Prof Hadley Wickham
+  "dplyr",      # Great for manipulating data, by Adj. Prof Hadley Wickham
+  "tidyr",      # Great for tidying data, by Adj. Prof Hadley Wickham
+  "stringr",    # Great for woking with strings, by Adj. Prof Hadley Wickham
+  "lubridate",  # Great for working with dates, by Adj. Prof Hadley Wickham
+  "readxl",     # Great for reading .xls, .xlsx, by Adj. Prof Hadley Wickham 
+  "devtools",   # Great for developing, by Adj. Prof Hadley Wickham 
+  "rstudioapi"  # For setting working directory
+)
+
+#--- Install Missing Packages ----
+# install.packages("tidyverse")
+# Check if any packages need to be installed
+new.packages <- packages[!(packages %in% installed.packages()[, "Package"])]
+# Install missing packages
+if(length(new.packages)) install.packages(new.packages)
+
+#--- Load Libraries ----
+invisible(lapply(packages, library, character.only = TRUE))
+# Clean up
+rm(packages, new.packages)
