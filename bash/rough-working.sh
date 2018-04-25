c00ea493d5dd00d53fbbcffa605dfb2c78bf199f
Alwin <alwang18@hotmail.com>
2018-04-25 19:10:23 +1000

    Bash dns and csplit script complete

--- bash/rough-working.sh
+++ bash/rough-working.sh
@@ -26,12 +26,12 @@ for sessionfile in $sessionfiles; do
     cp $sessionfile "$f"                    && echo "   Copy Made"          &&
     # Generate the mesh
     meshpr $f > "$f.msh"                    && echo "   Meshpr Finished"    &&
-    # Generate wall mesh
-    wallmesh $f "$f.msh" > "$f.wallmsh"     && echo "   Wall Mesh Finished" &&
     # Enumerate
     enumerate $f > "$f.num"                 && echo "   Enumerate Finished" &&
     # Compare
     compare $f > "$f.rst"                   && echo "   Compare Finished"   &&
+    # Generate wall mesh
+    wallmesh $f "$f.msh" > "$f.wallmsh"     && echo "   Wall Mesh Finished" &&
     # dns (verbosity -v turned off);
     # use > /dev/null to hide stdout and 2>&1 to hide stderror as well
     dns $f | grep "Divergence Energy:"      && echo "   DNS Finished"       &&
@@ -39,7 +39,12 @@ for sessionfile in $sessionfiles; do
     # Note: new field file CANNOT be the same as the original one (else 0)
     addfield -v -s $f "$f.fld" > "$f.vfld"  && echo "   Vorticity Added"    &&
     # Convert to ascii
-    convert "$f.vfld" > "$f.dump"            && echo "   Convert Finished"
+    convert "$f.vfld" > "$f.dump"           && echo "   Convert Finished"   &&
+    # Split the output file
+    csplit -z "$f.dump" /Session/ {*}       && echo "   Split Finished"     &&
+    # Rename split files
+    for i in [xx]*; do 
+      mv $i "$f${i#*xx}-.dump"; done         && echo "   Renamed Finished"
 done
 
 # Return to previous wd
