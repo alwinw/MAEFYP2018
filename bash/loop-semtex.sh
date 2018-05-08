9b0147099240a841f74476b06932776f7849353f
Alwin <alwang18@hotmail.com>
2018-05-08 19:11:03 +1000

    Bash loop complete

--- bash/loop-semtex.sh
+++ bash/loop-semtex.sh
@@ -41,11 +41,41 @@ for airfoil in airfoils; do
   for sessionfile in $sessionfiles; do
     echo "#----------------------------"    
     # Remove extension
-    f="${sessionfile%.*}"                     && echo "   Found $f"
+    f="${sessionfile%.*}"                               && echo "   Found $f"             &&
+      # Copy to extensionless file
+      cp $sessionfile "$f"                              && echo "   > Copy Made"          &&
+      # Generate the mesh
+      meshpr $f > "$f.msh"                              && echo "   > Meshpr Finished"    &&
+      # Add extra mesh info
+      meshpr -i $f > "$f.mshi"                          && echo "   > Meshpr -i Finished" &&
+      # Enumerate
+      enumerate $f > "$f.num"                           && echo "   > Enumerate Finished" &&
+      # Compare
+      compare $f > "$f.rst"                             && echo "   > Compare Finished"   &&
+      # Generate wall mesh
+      wallmesh $f "$f.msh" > "$f.wallmsh"               && echo "   > Wall Mesh Finished" &&
+      # dns (verbosity -v turned off);
+      # use > /dev/null to hide stdout and 2>&1 to hide stderror as well
+      dns $f | grep "Divergence Energy:"                && echo "   > DNS Finished"       &&
+      # Add vorticity field t
+      # Note: new field file CANNOT be the same as the original one (else 0)
+      addfield -v -s $f "$f.fld" > "$f.vfld"            && echo "   > Vorticity Added"    &&
+      # Convert to ascii
+      convert "$f.vfld" > "$f.flddump"                  && echo "   > Convert Finished"   &&
+      # Split the output file
+      csplit -z "$f.flddump" /Session/ {*} >/dev/null   && echo "   > Split Finished"     &&
+      # Remove unneded dump file
+      rm "$f.flddump"                                   && echo "   > ASCII dump removed" &&
+      # Rename split files
+      for i in [xx]*; do 
+        mv $i "$f-${i#*xx}.dump"; done                  && echo "   > Renamed Finished"
   done
   # Return to previous folder
   cd ..
 done
 
 #--- Return to previous wd ----
+echo 
+echo "#============================"
+echo 
 cd $oldpwd
