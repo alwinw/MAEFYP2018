8634739f58860cebcb800a1cd3feb78b74f47150
Alwin <alwang18@hotmail.com>
2018-05-13 21:25:24 +1000

    R initial plot exports

---
+++ R/src_plot-output.R
@@ -0,0 +1,8 @@
+#============================#
+# Plot Output
+# Alwin Wang
+#----------------------------#
+
+# Custom ggplot2 setup
+theme_set(theme_bw())                                           # Set black and white theme
+spectralpalette <- colorRampPalette(rev(brewer.pal(11, "Spectral")))
