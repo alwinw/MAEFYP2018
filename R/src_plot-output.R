927f668cb5677ce3e0ded24fa5f56d4abb0248eb
Alwin <alwang18@hotmail.com>
2018-05-26 15:44:50 +1000

    R applied finite difference bug fix

--- R/src_plot-output.R
+++ R/src_plot-output.R
@@ -51,7 +51,7 @@ PlotNS <- function(dump, dumpval, long, save = TRUE, saveplot = NULL) {
     geom_path(aes(y = - accels + dpds, colour = "LHS"),           # LHS, acceleration + pressure
               linetype = "dashed") +
     geom_point(aes(s, t_enum_diff*dump$kinvis, colour = "RHS"),   # RHS, v * dw/dz
-               dump$offset, shape = "O") +
+               dump$offset, shape = "o", alpha = 0.3) +
     xlab("s") + 
     scale_x_continuous(breaks = plot_xbreaks, 
                        labels = function(x) sprintf("%.2f", x)) +
@@ -70,7 +70,8 @@ PlotNS <- function(dump, dumpval, long, save = TRUE, saveplot = NULL) {
       guide = guide_legend(
         override.aes = list(
           linetype = c(rep("solid", 2), "dashed", "blank"),
-          shape = c(rep(NA, 3), "O")))) +
+          shape = c(rep(NA, 3), "O"),
+          alpha = rep(1, 4)))) +
     theme(legend.key.size = unit(2.25, "lines"),
           legend.text.align = 0.5,
           legend.direction = "vertical", 
@@ -119,46 +120,36 @@ PlotAccel <- function(dump, dumpval, save = TRUE, saveplot = NULL) {
   return(NULL)
 }
 
-
 #--- Plot Leading Edge ----
 PlotLE <- function(dump, dumpval, save = TRUE, saveplot = NULL) {
-  # Mesh to interpolate over
+  # Determine correct element for points for interpolation
   n = 250
-  plot_le_df = data.frame(
-    x = rep(seq(-0.455, -0.245, length.out = n), each = n),
-    y = rep(seq(-0.07, 0.1, length.out = n), times = n))
-  #--- Relevant enum ----
-  # Original mesh
+  # plot_limits <- data.frame(
+  #   x = c(-0.55, 0.65),
+  #   y = c(-0.3, 0.3))
+  plot_limits <- data.frame(
+    x = c(-0.455, -0.245),
+    y = c(-0.07, 0.1))
+  pts_df = data.frame(
+    x = rep(seq(plot_limits$x[1], plot_limits$x[2], length.out = n), each = n),
+    y = rep(seq(plot_limits$y[1], plot_limits$y[2], length.out = n), times = n))
   poly_df <- dump$threaddata %>% 
-    filter(x >= -0.8, x <= 0.1,
-           y >= -0.3, y <= 0.5,
-           seshnode) %>%
+    filter(seshnode) %>%
     arrange(enum, ncorner) %>%
     select(x, y, enum)
-  pts_df <- plot_le_df
-  # Split the dataframe into a list based on enum and then remove enum from df in the list
-  poly_list <- split(poly_df, poly_df$enum)
-  # Convert the list to Polygon, then create a Polygons object
-  poly_sp <- sapply(poly_list, function(poly){Polygons(list(Polygon(poly[, c("x", "y")])), ID = poly[1, "enum"])})
-  poly_sp <- SpatialPolygons(poly_sp)
-  # Convert points to coordinates
-  pts_ps <- pts_df
-  coordinates(pts_ps) <- ~x+y
-  # Determine polygons points are in
-  pts_return <- over(pts_ps, poly_sp, returnList = FALSE)
-  plot_le_df$enum <- unique(poly_df$enum)[pts_return] 
-  plot_le_df <- filter(plot_le_df, !is.na(enum))
-  plot_le_list <- split(plot_le_df, plot_le_df$enum)
-  #--- Threaddata for enum ----
+  pts_df <- PointinElement(pts_df, poly_df)
+  
+  # Threaddata for enum
+  pts_list <- split(pts_df, pts_df$enum)
   plot_full_df <- dump$threaddata %>%
-    filter(enum %in% names(plot_le_list)) %>%
+    filter(enum %in% names(pts_list)) %>%
     select(x, y, t, enum, nnum, ncorner, local)
   plot_full_list <- split(plot_full_df, plot_full_df$enum)
   
-  plot_le_interp <- lapply(names(plot_le_list), function(enum) {
+  plot_le_interp <- lapply(names(pts_list), function(enum) {
     plot_le_interp <- as.data.frame(
       interpp(x = plot_full_list[[enum]]$x, y = plot_full_list[[enum]]$y, z = plot_full_list[[enum]]$t,
-              xo = plot_le_list[[enum]]$x, yo = plot_le_list[[enum]]$y,
+              xo = pts_list[[enum]]$x, yo = pts_list[[enum]]$y,
               linear = TRUE,
               duplicate = "strip"))
     return(plot_le_interp)
@@ -166,32 +157,43 @@ PlotLE <- function(dump, dumpval, save = TRUE, saveplot = NULL) {
   
   plot_le_interp <- bind_rows(plot_le_interp) %>%
     filter(!is.na(z))
-  
-  # plot_le_interp <- as.data.frame(
-  #   interpp(x = plot_full_df$x, y = plot_full_df$y, z = plot_full_df$t,
-  #           xo = plot_le_df$x, yo = plot_le_df$y,
-  #           linear = TRUE,
-  #           duplicate = "strip"))
   colnames(plot_le_interp) <- c("x", "y", "t")
   
+  PlotScaleLimit <- function(t, max) ifelse(abs(t) > max, max*sign(t), t)
+  PlotContourBreak <- function(max, step) seq(-max, max, by = step)[seq(-max, max, by = step) != 0]
+  
   plot_le <- ggplot(plot_le_interp) +
-    geom_tile(aes(x, y, fill = ifelse(abs(t) > 200, 200*sign(t), t))) +
-    # stat_contour(aes(x, y, z = t, fill = ..level..), geom = "polygon",
-                 # bins = 10) +
-    geom_point(aes(x, y, fill = ifelse(abs(t) > 200, 200*sign(t), t), colour = local), 
-               data = plot_full_df,
-               shape = 21) +
-    geom_polygon(aes(x, y, group = enum, colour = local), fill = NA, alpha = 0.1, linetype = "dotted",
+    # Vorticity tile
+    geom_tile(aes(x, y, fill = PlotScaleLimit(t, 200)), alpha = 1) +
+    # Original Points
+    # geom_point(aes(x, y, fill = PlotScaleLimit(t, 200)), 
+    # shape = 23, colour = "white",
+    # data = plot_full_df) +
+    # Original Mesh
+    geom_polygon(aes(x, y, group = enum), 
+                 fill = NA, colour = "grey90", alpha = 0.1, linetype = "1616",
                  data = plot_full_df %>% filter(!is.na(nnum)) %>% arrange(enum, ncorner)) +
+    # Vorticity contours
+    # stat_contour(aes(x, y, z = t, fill = ..level..), geom = "polygon", alpha =1,
+    # breaks = PlotContourBreak(200, 10)) +
+    stat_contour(aes(x, y, z = t, colour = ..level..), alpha = 1,
+                 breaks = PlotContourBreak(200, 10)) +
+    # stat_contour(aes(x, y, z = t, colour = -abs(..level..)),
+                 # breaks = PlotContourBreak(200, 10)) +
+    # Chosen offset points
+    # geom_point(aes(x, y, fill = PlotScaleLimit(t_enum, 200)),
+    #            data = dump$offset,
+    #            shape = 21) +
+    # Airfoil
     geom_polygon(aes(x, y), fill = "white", colour = "black", alpha = 1,
                  data = dump$accel) +
     coord_fixed(
-      xlim = c(-0.45, -0.25),
-      ylim = c(-0.055, 0.090),
+      xlim = plot_limits$x,
+      ylim = plot_limits$y,
       expand = FALSE) +
     scale_fill_gradientn(name = "vorticity\n", colours = spectralpalette(20), limits = c(-200, 200)) +
-    # scale_colour_gradientn(colours = spectralpalette(20), limits = c(-150, 150))
-    scale_colour_gradient(low = "grey99", high = "grey80", guide = "none")
+    scale_colour_gradientn(colours = spectralpalette(20), limits = c(-200, 200), guide = "none")
+    # scale_colour_continuous(low = "grey90", high = "white", guide = "none")
   
   plot_filename <- paste(
     dumpval$ID,
@@ -209,6 +211,77 @@ PlotLE <- function(dump, dumpval, save = TRUE, saveplot = NULL) {
            width = 6, height = 5)
   } else {
     print(plot_le)
+  }
+  # Return
+  return(NULL)
+}
+
+
+#--- Plot LHS RHS Error ----
+PlotError <- function(dump, surface, dumpval, long, save = TRUE, saveplot = NULL) {
+  summary <- data.frame(
+    x = c(2.5, 3),
+    y = rep(-90, 2),
+    labels = c(paste("median:", sprintf("%.2f%%", median(filter(surface, up)$error)), "\n",
+                     "iqr:", sprintf("%.2f%%", IQR(filter(surface, up)$error))),
+               paste("median:", sprintf("%.2f%%", median(filter(surface, !up)$error)), "\n",
+                     "iqr:", sprintf("%.2f%%", IQR(filter(surface, !up)$error)))))
+  
+  plot_vline <- data.frame(                                       # LE, TE, LE limits for vertical lines
+    te_up = min(long$walldata$s),
+    le = long$walldata[long$walldata$theta == pi,]$s[1],
+    te_lo = max(long$walldata$s))
+  plot_surf <- data.frame(                                        # Labels for surfaces
+    x = c(plot_vline$te_up,
+          (plot_vline$te_up + plot_vline$le)/2,
+          plot_vline$le,
+          (plot_vline$le + plot_vline$te_lo)/2,
+          plot_vline$te_lo),
+    y = rep(-100, 5),
+    labels = c("TE", "Upper Surface", "LE", "Lower Surface", "TE"))
+  plot_xbreaks <-                                                 # Breaks in x axis
+    c(seq(plot_vline$te_up, plot_vline$le, length.out = 3)[1:2],
+      seq(plot_vline$le, plot_vline$te_lo, length.out = 3))
+  plot_xlabs <- sprintf("%.2f", plot_xbreaks)
+  plot_xbreaks <- c(plot_xbreaks, 2.5, 3)
+  plot_xlabs <- c(plot_xlabs, "Uppper", "Lower")
+  
+  ploterror <- ggplot(surface) +
+    geom_vline(xintercept = as.numeric(plot_vline),               # Vertical lines for LE, TE, LE
+               colour = "grey", linetype = "dashed") +
+    geom_col(aes(s, error, fill = up)) +
+    geom_boxplot(aes(as.numeric(up)*-0.5 + 3, error, colour = up),
+                 outlier.shape = "x") +
+    geom_label(aes(x, y, label = labels), plot_surf,              # Surface labels
+               colour = "grey40", size = 3) +
+    geom_label(aes(x, y, label = labels), summary,                # Summary labels
+               colour = "grey40", size = 3) +
+    scale_x_continuous(breaks = plot_xbreaks, 
+                       labels = plot_xlabs) +
+    coord_cartesian(ylim = c(-100, 100)) +
+    ylab(expression(frac(RHS - LHS, RHS)%*%100~"%")) +
+    guides(fill = FALSE, colour = FALSE) +
+    theme(
+      axis.title.y = element_text(margin = margin(t = 0, r = -10, b = 0, l = 0)),
+      axis.title.x = element_text(margin = margin(t = 0, r = 10000, b = 0, l = 0))
+      )
+  
+  plot_filename <- paste(
+    dumpval$ID,
+    paste0("v", format(dump$kinvis, scientific = TRUE)),
+    paste0("t", sprintf("%05.3f", dump$time)),
+    paste0("a", sprintf("%+07.3f", dump$acceleration)),
+    "err",
+    sep = "_")
+  
+  # Save plot
+  if (save) {
+    plotpath = paste0(saveplot, "/", dumpval$airfoil, "_", dumpval$seshname)
+    if (!dir.exists(plotpath)) dir.create(plotpath, recursive = TRUE)
+    ggsave(paste0(plot_filename, ".png"), ploterror, path = plotpath,
+           width = 8, height = 3)
+  } else {
+    print(ploterror)
   }
   # Return
   return(NULL)
