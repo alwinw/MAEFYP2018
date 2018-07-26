417001f19bd7688d23f0bc26315dcebf3ef90d11
Alwin <alwang18@hotmail.com>
2018-07-26 15:29:47 +1000

    Semtex naca0012 analysis with addvortfield
    Too slow!

---
+++ src-example/NACA0012/naca0012.sh
@@ -0,0 +1,20 @@
+# Create required setup files
+# Run dns on session file
+for sessionfile in *.sesh; do
+  f="${sessionfile%.*}"                   &&
+  cp $sessionfile  results/$f             &&
+  cp bndry_prf.dat results/bndry_prf.dat  &&
+  cd results                              &&
+  meshpr -i $f > $f.mshi                  &&
+  enumerate $f > $f.num                   &&
+  dns $f | grep "Divergence Energy:"      &&
+  addvortfield -G -s $f $f.fld > $f.Gfld  &&
+  convert $f.Gfld > $f.flddump            &&
+  compare $f $f.fld > /dev/null           &&
+  csplit -z "$f.flddump" /Session/ '{*}' >/dev/null &&
+  for i in [xx]*; do 
+    mv $i "$f-${i#*xx}.dump"; done                  &&
+  echo $sessionfile complete
+  cd ..
+done
+
