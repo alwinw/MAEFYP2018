afac9de2a22ad6b51f6aacd92fadcbeeef96bf9a
Alwin <alwang18@hotmail.com>
2018-07-26 10:45:15 +1000

    Git merge script

--- git_update.sh
+++ git_update.sh
@@ -1,13 +1,14 @@
-git checkout dev/bash_runs
-git merge master
-git checkout dev/cpp_semtex
-git merge master
-git checkout dev/R_airfoil-analysis
-git merge master
-git checkout dev/R_batch-analysis
-git merge master
-git checkout dev/R_single-analysis
-git merge master
-git checkout wip/doc_reports
-git merge master
-git push --origin
+git checkout dev/bash_runs          &&
+git merge master  &&
+git checkout dev/cpp_semtex         &&
+git merge master  &&
+git checkout dev/R_airfoil-analysis &&
+git merge master  &&
+git checkout dev/R_batch-analysis   &&
+git merge master  &&
+git checkout dev/R_single-analysis  &&
+git merge master  &&
+git checkout wip/doc_reports        &&
+git merge master  &&
+git checkout master                 &&
+git push
