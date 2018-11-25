ff6802c7e9d679387accd9059ca43c344cbc7bd3
Alwin <16846521+AlwinW@users.noreply.github.com>
2018-11-25 16:36:27 +1100

    NACA lower surface incorrect

--- analysis/plate_mesh/wip_naca.R
+++ analysis/plate_mesh/wip_naca.R
@@ -3,6 +3,8 @@
 # Alwin Wang
 #----------------------------#
 
+# LOWER SURFACE IS NOT CORRECT FOR CAMBERED AIRFOILS
+
 #--- Set Up ----
 # Use rstudioapi to get saved location of this file
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))     # Requres devtools, rstudioapi
@@ -16,12 +18,11 @@ spectralpalette <-                                              # Custom spectra
 
 #--- Inputs ----
 input <- list(
-  x = data.frame(min = -1.75, max = 5.4375, n = 21), #51
-  y = data.frame(min = -1.25, max = 1.25,   n = 11), #21
+  x = data.frame(min = -1.75, max = 5.4375, n = 61, polyn = 4), #51
+  y = data.frame(min = -1.25, max = 1.25,   n = 21, polyn = 3), #21
   t = 0.12,
   alpha = 4, # degrees
-  polyn = 2,
-  NACA = 2412
+  NACA = 0012
 )
 
 #--- Polynomial ----
@@ -31,15 +32,15 @@ c = data.frame(x = 0.5*cos(input$alpha*pi/180),
 dx = (input$x$max - input$x$min)/input$x$n
 listx = list(
   LE = seq(input$x$min, -c$x, length.out = round((-c$x - input$x$min)/dx) + 1),
-  CD = seq(-c$x, c$x, length.out = 3*round(c$x*2/dx) + 1),
+  CD = seq(-c$x, c$x, length.out = 2*round(c$x*2/dx) + 1),
   TE = seq(c$x, -input$x$min, length.out = round((-input$x$min - c$x)/dx) + 1) )
 polyx = list(
-  LE = polypow(c(1,  c$x), input$polyn + 1), 
-  CD = polyint(polymul(polypow(c(1,-c$x),input$polyn), polypow(c(1,c$x),input$polyn))),
-  TE = polypow(c(1, -c$x), input$polyn + 1) )
-polyx$LE = polyadd(polyx$LE/((input$x$min + c$x)^input$polyn), c(-c$x))
+  LE = polypow(c(1,  c$x), input$x$polyn + 1), 
+  CD = polyint(polymul(polypow(c(1,-c$x),input$x$polyn), polypow(c(1,c$x),input$x$polyn))),
+  TE = polypow(c(1, -c$x), input$x$polyn + 1) )
+polyx$LE = polyadd(polyx$LE/((input$x$min + c$x)^input$x$polyn), c(-c$x))
 polyx$CD = c$x/(polyval(polyx$CD, c$x)) * polyx$CD
-polyx$TE = polyadd(polyx$TE/((-input$x$min - c$x)^input$polyn), c( c$x))
+polyx$TE = polyadd(polyx$TE/((-input$x$min - c$x)^input$x$polyn), c( c$x))
 outpx = list(
   LE = polyval(polyx$LE, listx$LE),
   CD = polyval(polyx$CD, listx$CD),
@@ -53,10 +54,10 @@ listy = list(
   UP = seq(c$y, input$y$max, length.out = round(input$y$n/2) + 1) )
 
 polyy = list(
-  LO = polypow(c(1, -c$y), input$polyn + 1), 
-  UP = polypow(c(1, -c$y), input$polyn + 1) )
-polyy$LO = polyadd(polyy$LO/((input$y$max + c$y)^input$polyn)*(-2*mod(input$polyn, 2) + 1), c(c$y))
-polyy$UP = polyadd(polyy$UP/((input$y$max - c$y)^input$polyn), c(c$y))
+  LO = polypow(c(1, -c$y), input$y$polyn + 1), 
+  UP = polypow(c(1, -c$y), input$y$polyn + 1) )
+polyy$LO = polyadd(polyy$LO/((input$y$max + c$y)^input$y$polyn)*(-2*mod(input$y$polyn, 2) + 1), c(c$y))
+polyy$UP = polyadd(polyy$UP/((input$y$max - c$y)^input$y$polyn), c(c$y))
 outpy = list(
   LO = polyval(polyy$LO, listy$LO),
   UP = polyval(polyy$UP, listy$UP) )
@@ -150,16 +151,42 @@ Airfoilx <- function(NACA, xO, surf = "upper", a = -0.5, c = 1, tol = 1e-9, out 
     return(str(rootfind))
 }
 
-if(FALSE) {
-  airfoil <- data.frame(x = seq(0.5, -0.5, -0.001))
-  airfoil$x0U <- sapply(airfoil$x, function(x) Airfoilx(input$NACA, x, "upper"))
-  airfoil$x0L <- sapply(airfoil$x, function(x) Airfoilx(input$NACA, x, "lower"))
-  airfoil$yU  <- AirfoilCurve(input$NACA, airfoil$x0U)$yU
-  airfoil$yL  <- AirfoilCurve(input$NACA, airfoil$x0U)$yL
-  airfoil <- data.frame(
-    x = c(airfoil$x, rev(airfoil$x)),
-    y = c(airfoil$yU, rev(airfoil$yL))
+Airfoilxd <- function(NACA, alpha, x0, surf = "upper", a = -0.5, c = 1, tol = 1e-9, out = "x") {
+  rootfind <- uniroot(
+    function(x) {
+      local  = AirfoilCurve(NACA, x, a, c, out = surf)
+      r.x    = local$x*cos(alpha) + local$y*sin(alpha)
+      return(r.x - x0) },
+    lower = a, upper = a + c,
+    tol = tol
   )
+  if(out == "x")
+    return(rootfind$root)
+  else if(out ==  "all")
+    return(rootfind)
+  else if(out == "str")
+    return(str(rootfind))
+}
+
+xa = -0.5; xc = 1
+xvec = seq(0.5, -0.5, -0.025)
+xvec = -2*xa/xc^3 * (xvec - xa)^3 + xa
+airfoil <- data.frame(x = xvec)
+airfoil$x0U <- sapply(airfoil$x, function(x) Airfoilx(input$NACA, x, "upper"))
+airfoil$x0L <- sapply(airfoil$x, function(x) Airfoilx(input$NACA, x, "lower"))
+airfoil$yU  <- AirfoilCurve(input$NACA, airfoil$x0U)$yU
+airfoil$yL  <- AirfoilCurve(input$NACA, airfoil$x0U)$yL
+airfoil <- data.frame(
+  x0 = c(airfoil$x, rev(airfoil$x)[-1]),
+  y0 = c(airfoil$yU, rev(airfoil$yL)[-1])
+)
+airfoil <- airfoil %>%  
+  mutate(
+    x =   x0*cos(input$alpha*pi/180) + y0*sin(input$alpha*pi/180),
+    y = - x0*sin(input$alpha*pi/180) + y0*cos(input$alpha*pi/180) ) %>% 
+  select(-x0, -y0)
+
+if(FALSE) {
   ggplot(airfoil, aes(x, y)) +
     geom_path() +
     coord_fixed()
@@ -181,20 +208,20 @@ AirfoilTrans <- function(n1) {
   if (n1$bcx == "CD") {
     # Add on airfoil thickness
     alpha = input$alpha*pi/180
-    x     = n1$x/cos(alpha)
-    y     = n1$y
-    y0    = -x*tan(alpha)
-    ym    = input$y$max
+    x     = n1$x             # x location of current node
+    y     = n1$y             # y location of current node
+    y0    = -n1$x*tan(alpha) # y location of chord line
+    ym    = input$y$max*0.8  # maximum y value
     if (n1$bcy == "UP" | n1$bcy == "CD") {
-      x0 = Airfoilx(input$NACA, x, "upper")
-      ys = AirfoilCurve(input$NACA, x0)$yU
-      dy = ys*(cos(pi/(2*(ym-y0))*(y-y0))^2)
+      x0 = Airfoilxd(input$NACA, alpha, x, "upper")      # x value on chord line for x value on surface
+      ys = AirfoilCurve(input$NACA, x0)$yU/cos(alpha)
+      dy = ys*(cos(pi/(2*ym)*(y-y0)))^2
       n1$y = n1$y + dy
     }
     if (n1$bcy == "LO") {
-      x0 = Airfoilx(input$NACA, x, "lower")
-      ys = AirfoilCurve(input$NACA, x0)$yL
-      dy = ys*(cos(pi/(2*(ym-y0))*(y+y0))^2)
+      x0 = Airfoilxd(input$NACA, alpha, x, "lower")
+      ys = AirfoilCurve(input$NACA, x0)$yL/cos(alpha)
+      dy = ys*(cos(pi/(2*ym)*(y-y0)))^2
       n1$y = n1$y + dy
     }
   }
@@ -240,11 +267,6 @@ for (j in 1:(ny - 1)) {
     n2 = data.frame(x = dfx$x[i  ], y = dfy$y[j+1], nnum = matnnum[j+1, i  ], bcx = dfx$bcx[i  ], bcy = dfy$bcy[j+1])
     n3 = data.frame(x = dfx$x[i+1], y = dfy$y[j+1], nnum = matnnum[j+1, i+1], bcx = dfx$bcx[i+1], bcy = dfy$bcy[j+1])
     n4 = data.frame(x = dfx$x[i+1], y = dfy$y[j  ], nnum = matnnum[j  , i+1], bcx = dfx$bcx[i+1], bcy = dfy$bcy[j  ])
-    # Determine y values based on bcx
-    # n1 = flags(n1, i  , j  )
-    # n2 = flags(n2, i  , j+1)
-    # n3 = flags(n3, i+1, j+1)
-    # n4 = flags(n4, i+1, j  )
     # Determine wall bcs
     bc1 = NA; bc2 = NA; bc3 = NA; bc4 = NA;
     if (j == 1) {         # TOP | side 4
@@ -273,7 +295,6 @@ for (j in 1:(ny - 1)) {
           i = i, j = j, surf = surf, enum = enum, side = 2, bc = bc2, nnum1 = n2$nnum, nnum2 = n3$nnum)) }
     }
     # Side 4 airfoil bc
-    addnode = FALSE
     if (n4$bcy == "CD" & n1$bcy == "CD") {
       if (n4$bcx %in% c("CD", "TE")) { # Element below plote | side 4
         # if (n1$bcx == "CD") {n1$bcy = "LO"}
@@ -281,8 +302,14 @@ for (j in 1:(ny - 1)) {
         # Add extra nodes as required
         if (n4$bcx == "CD") {
           nnum = nrow(dfnode) + 1
-          addnode = TRUE
           n4$nnum = nnum
+          n4$bcy = "LO"
+          n4 = flags(n4, i+1, j)
+          n4 = AirfoilTrans(n4)
+          dfnode <- rbind(dfnode, data.frame(
+            nnum = nnum,
+            x = n4$x, y = n4$y, z = 0, bcx = "padd", bcy = "LO") )
+          
           if (n1$bcx == "CD") {
             n1$nnum = nnum - 1
           }
@@ -294,22 +321,6 @@ for (j in 1:(ny - 1)) {
       }
     }
     
-    # Transform y for the airfoil thickness
-    # n1 = AirfoilTrans(n1)
-    # n2 = AirfoilTrans(n2)
-    # n3 = AirfoilTrans(n3)
-    # n4 = AirfoilTrans(n4)
-    
-    # Node Result
-    if (addnode) {
-      n4$bcy = "LO"
-      n4 = flags(n4, i+1, j)
-      n4 = AirfoilTrans(n4)
-      dfnode <- rbind(dfnode, data.frame(
-        nnum = nnum,
-        x = n4$x, y = n4$y, z = 0, bcx = "padd", bcy = "LO") )
-    }
-    
     # Element Result
     dfrow = data.frame(
       enum    = enum,
@@ -317,41 +328,87 @@ for (j in 1:(ny - 1)) {
       j       = c(j, j  , j+1, j+1),
       nnum    = c(n1$nnum, n2$nnum, n3$nnum, n4$nnum),
       ncorner = c("n1", "n2", "n3", "n4"),
-      # x       = c(n1$x, n2$x, n3$x, n4$x),
-      # y       = c(n1$y, n2$y, n3$y, n4$y),
       bc      = c(bc1, bc2, bc3, bc4) )
     dfelem <- rbind(dfelem, dfrow)
   }
 }
 
 #--- Plot ----
+#--- * Airfoil ----
+plotaf <- ggplot(airfoil, aes(x, y)) + geom_path()
+plotaf + coord_fixed()
+plotaf + 
+  geom_point() + 
+  geom_point(data = mutate(c, x = -x), colour = "red") +
+  coord_fixed(xlim = c(-0.55, -0.45), ylim = c(-0.02, 0.08))
 #--- * Nodes ----
 plotnode <- ggplot(dfnode, aes(x, y)) +
+  geom_path(aes(x, y), airfoil) +
+  geom_point(aes(x, y), airfoil, shape = "o") +
   geom_point(aes(colour = nnum)) +
   scale_colour_gradientn(colours = spectralpalette(10)) +
   ggtitle(paste(paste0("NACA", input$NACA), "|", 
                 input$alpha, "deg", "|", 
-                "poly =", input$polyn, "|",
+                "polyx =", input$x$polyn, "|",
+                "polyy =", input$y$polyn, "|",
                 "xn =", input$x$n, "|",
                 "yn =", input$y$n, "|"))
 plotnode +
   coord_fixed(xlim = c(-0.5, 0.5), ylim = c(-0.3, 0.3))
+plotnode +
+  coord_fixed(xlim = c(-0.51, -0.40), ylim = c(-0.02, 0.08))
+
+#--- * Surfaces ----
+# Create a dataframe of (x,y) to ggplot
+plotsurf <- data.frame()
+for (i in 1:nrow(dfsurf)) {
+  dfrow = data.frame(
+    bc   = as.character(dfsurf$bc[i]),
+    side = dfsurf$side[i],
+    node = c(dfsurf$nnum1[i], dfsurf$nnum2[i]),
+    x    = c(filter(dfnode, nnum == dfsurf$nnum1[i])$x, filter(dfnode, nnum == dfsurf$nnum2[i])$x),
+    y    = c(filter(dfnode, nnum == dfsurf$nnum1[i])$y, filter(dfnode, nnum == dfsurf$nnum2[i])$y),
+    stringsAsFactors = FALSE)
+  plotsurf <- rbind(plotsurf, dfrow)
+}
+plotsurf$side <- as.factor(plotsurf$side)
+plotsurf <- plotsurf %>% arrange(node)
+plotsurf <- plotsurf %>%  mutate(
+  y  = ifelse(node > max(matnnum), y - 0.01*(input$NACA == 0), y),
+  bc.side = paste(bc, side, sep = "."))
 
 
-# plotmesh <- ggplot(dfelem, aes(x, y, group = enum)) +
-#   geom_polygon(aes(colour = enum), fill = NA) +
-#   # geom_path(aes(colour = bc, group = bc), dfelem %>% filter(!is.na(bc))) +
-#   geom_point(data = mutate(c, x = -x)) +
-#   geom_point(data = mutate(c, y = -y)) + 
-#   scale_colour_gradientn(colours = spectralpalette(10))
-# 
-# # plotmesh +
-#   # coord_fixed(xlim = c(0.49855, 0.49910), ylim = c(-0.03505, -0.03470))
-# 
-# # plotmesh +
-# #   coord_fixed(xlim = c(0.49750, 0.50000), ylim = c(-0.03570, -0.03420))
-# 
-# ggplot(dfelem %>% filter(i <= 10, j <= 8), aes(x, y, group = enum)) +
-#   geom_polygon(aes(colour = enum), fill = NA) +
-#   coord_fixed(xlim = c(-0.5, 0.5), ylim = c(-0.3, 0.3)) + 
-#   scale_colour_gradientn(colours = spectralpalette(10))
+plotbc <- ggplot(plotsurf, aes(x, y)) +
+  geom_path(aes(x, y), airfoil) + 
+  geom_line(aes(colour = bc.side)) +
+  geom_point(aes(shape = bc.side, colour = bc.side))
+
+plotbc + coord_fixed()
+plotbc + coord_fixed(xlim = c(-0.5, 0.5), ylim = c(-0.3, 0.3)) 
+
+#--- * Mesh ----
+plotelem <- left_join(dfelem, dfnode, by = "nnum")
+plotmesh <- ggplot(plotelem, aes(x, y, group = enum)) +
+  geom_path(aes(x, y), airfoil) +
+  geom_polygon(aes(colour = enum), fill = NA) +
+  geom_point(data = mutate(c, x = -x)) +
+  geom_point(data = mutate(c, y = -y)) +
+  scale_colour_gradientn(colours = spectralpalette(10))
+
+plotmesh + 
+  coord_fixed(expand = FALSE)
+
+plotmesh + 
+  coord_fixed(xlim = c(-0.51, -0.40), ylim = c(-0.02, 0.08), expand = FALSE)
+
+plotmesh + 
+  coord_fixed(xlim = c(0.40, 0.51), ylim = c(-0.08, 0.02), expand = FALSE)
+
+plotmesh +
+  coord_fixed(xlim = c(0.49855, 0.49910), ylim = c(-0.03505, -0.03470))
+
+plotmesh +
+  coord_fixed(xlim = c(0.49750, 0.50000), ylim = c(-0.03570, -0.03420))
+
+plotmesh +
+  coord_cartesian(xlim = c(-0.4990, -0.498), ylim = c(0.031, 0.038))
